f9d4287b8d86d7417e366b24654e3508b5745c14
Thong Pham <thongpv87@gmail.com>
2023-05-20 17:01:08 +0700

    use pipewire replace pulseaudio

--- profiles/laptop/default.nix
+++ profiles/laptop/default.nix
@@ -8,24 +8,22 @@
   ];
 
   hardware = {
+    enableAllFirmware = true;
+
     pulseaudio = {
-      enable = true;
+      # enable = true;
       package = pkgs.pulseaudioFull;
       extraConfig = "load-module module-switch-on-connect";
 
       # configFile = pkgs.writeText "default.pa" ''
-      # load-module module-bluetooth-policy
-      # load-module module-bluetooth-discover
+      #   load-module module-bluetooth-policy
+      #   load-module module-bluetooth-discover
       # '';
     };
 
     bluetooth = {
       enable = true;
-      settings = {
-        General = {
-          Enable = "Source,Sink,Media,Socket";
-        };
-      };
+      settings = { General = { Enable = "Source,Sink,Media,Socket"; }; };
     };
   };
 
@@ -35,9 +33,7 @@
     description = "Mpris proxy";
     after = [ "network.target" "sound.target" ];
     wantedBy = [ "default.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
-    };
+    serviceConfig = { ExecStart = "${pkgs.bluez}/bin/mpris-proxy"; };
   };
 
   # to enable brightness keys 'keys' value may need updating per device
@@ -58,7 +54,28 @@
     ];
   };
 
-  sound.enable = true;
+  # PireWire
+  sound.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+  environment.etc = {
+    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+      bluez_monitor.properties = {
+        ["bluez5.enable-sbc-xq"] = true,
+        ["bluez5.enable-msbc"] = true,
+        ["bluez5.enable-hw-volume"] = true,
+        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+      }
+    '';
+  };
+
   sound.mediaKeys = lib.mkIf (!config.hardware.pulseaudio.enable) {
     enable = true;
     volumeStep = "1dB";
@@ -78,18 +95,16 @@
 
   services.logind.lidSwitch = "suspend";
 
-  nixpkgs.overlays =
-    let
-      light_ov = self: super: {
-        light = super.light.overrideAttrs (o: {
-          src = self.fetchFromGitHub {
-            owner = "haikarainen";
-            repo = "light";
-            rev = "ae7a6ebb45a712e5293c7961eed8cceaa4ebf0b6";
-            sha256 = "00z9bxrkjpfmfhz9fbf6mjbfqvixx6857mvgmiv01fvvs0lr371n";
-          };
-        });
-      };
-    in
-    [ light_ov ];
+  nixpkgs.overlays = let
+    light_ov = self: super: {
+      light = super.light.overrideAttrs (o: {
+        src = self.fetchFromGitHub {
+          owner = "haikarainen";
+          repo = "light";
+          rev = "ae7a6ebb45a712e5293c7961eed8cceaa4ebf0b6";
+          sha256 = "00z9bxrkjpfmfhz9fbf6mjbfqvixx6857mvgmiv01fvvs0lr371n";
+        };
+      });
+    };
+  in [ light_ov ];
 }
